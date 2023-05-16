e213beaf8003ceba01e62e5eee1ce77480a538ac
Yoav TC <yoavtc2004@gmail.com>
2023-05-16 18:06:47 +0300

    Added projects page & made it functional. Fixed a bunch of CSS issues

---
+++ index.js
@@ -0,0 +1,54 @@
+window.addEventListener('load', function() {
+    if (window.scrollY <= 0 && window.location.hash != "#back") {
+        MoveDown();
+        SetAge();
+    } 
+    else {
+        window.scrollTo(0, 1000);
+        ageTitle = document.getElementById('age');
+        text = ageTitle.textContent;
+        newText = text.replace(0, 18);
+        ageTitle.textContent = newText;
+    }
+});
+
+let dateOfBirth = '2004-11-12';
+let maxAge;
+let tempAge = 0;
+let intervalID;
+
+
+function MoveDown() {
+    setTimeout(function() {
+      window.scroll({
+        top: 1000,
+        behavior: 'smooth',
+        block: 'end'
+      });
+    }, 5500);
+}
+
+function GetAge(dateOfBirth) {
+    let now = new Date();
+    let birthDate = new Date(dateOfBirth);
+    let ageInMilliseconds = now - birthDate;
+    let ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+    return Math.floor(ageInYears);
+}
+
+function SetAge() {
+    intervalID = setInterval(LoopAge, 120)
+    maxAge = GetAge(dateOfBirth);
+}
+
+function LoopAge() {
+    ageTitle = document.getElementById('age');
+    text = ageTitle.textContent;
+    newText = text.replace(tempAge, tempAge + 1);
+    tempAge += 1;
+    ageTitle.textContent = newText;
+
+    if (tempAge == maxAge) {
+        clearInterval(intervalID);
+    } 
+}
--- pages/js/projects.js
+++ pages/js/projects.js
@@ -1,42 +1,38 @@
-//page load
+const gridParent = document.getElementById('grid-parent');
+
 window.onload = function() {
+    projectsList.sort((a, b) => new Date(a.date) - new Date(b.date));
     sortProjects();
 }
 
-//projects
-//TIME = int of approx days
-//DATE = int from 0 to 100
-//PRIDE = int from 0 to 100
-//POPULARITY = num of downloads / views
-//NAME = name of the project...     ._. duh
-var projectList = [
-    {time: 500, date: 1 , pride: 95, popularity: 2500, name: "CLAYTYNE Minecraft Server", link: "https://www.youtube.com/@claytyneserver3182"},
-    {time: 4, date: 15, pride: 25, popularity: 4243,  name: "Zombie Body Parts Resourcepack", link: "https://www.planetminecraft.com/texture-pack/zombie-body-parts/"},
-    {time: 2, date: 65, pride: 75, popularity: 3700,  name: "Rain Temple Datapack", link: "https://www.planetminecraft.com/data-pack/rain-temple/"},
-    {time: 1, date: 50, pride: 25, popularity: 1000,  name: "Simple Baking Datapack", link: "https://www.planetminecraft.com/data-pack/simple-baking/"},
-    {time: 3, date: 50, pride: 60, popularity: 1000,  name: "Halloween Datapack", link: "https://www.planetminecraft.com/data-pack/holiday-spirit-halloween-edition/"},
-    {time: 1, date: 71, pride: 65, popularity: 2000,  name: "Jumpscare Datapack", link: "https://www.planetminecraft.com/data-pack/jumpscare-traps-datapack/"},
-    {time: 15, date: 45, pride: 85, popularity: 52000,  name: "More Ice Mod", link: "https://www.curseforge.com/minecraft/mc-mods/more-ice"},
-    {time: 20, date: 85, pride: 90, popularity: 300,  name: "Vineyard Mod", link: "https://www.curseforge.com/minecraft/mc-mods/vineyard-mod"},
-    {time: 15, date: 88, pride: 60, popularity: 20,  name: "Mulberry Mod", link: "https://www.curseforge.com/minecraft/mc-mods/mulberry"},
-    {time: 1, date: 90, pride: 70, popularity: 20,  name: "Kill Me Not Mod", link: "https://www.curseforge.com/minecraft/mc-mods/kill-me-not"},
-    {time: 30, date: 91, pride: 77, popularity: 50,  name: "My YouTube", link: "https://www.youtube.com/@yoav_tc"},
-    {time: 31, date: 60, pride: 85, popularity: 100,  name: "Chef Shooter Game", link: "https://yoav-tc.itch.io/chef-shooter"},
-    {time: 18, date: 65, pride: 80, popularity: 400,  name: "Minecraft Wipeout", link: "https://www.reddit.com/r/MinecraftCommands/comments/xe7bg2/i_made_a_working_wipeout_course_for_a_youtube/?utm_source=share&utm_medium=web2x&context=3"},
-    {time: 20, date: 75, pride: 70, popularity: 200,  name: "Minecraft Album Covers", link: "https://www.reddit.com/r/Minecraft/comments/yqw87q/my_friends_and_i_recreated_a_bunch_of_album/"},
-    {time: 5, date: 55, pride: 45, popularity: 35,  name: "Valorant in Minecraft", link: "https://www.reddit.com/r/Minecraft/comments/mzq7av/i_created_jett_and_sova_from_valorant_in_minecraft/"},
-    {time: 150, date: 80, pride: 88, popularity: 500,  name: "YouTube Channel with Friends", link: "https://www.youtube.com/@AktimelGroup"},
-    {time: 4, date: 99, pride: 87, popularity: 0,  name: "Bamboo Flute Mod", link: "https://www.curseforge.com/minecraft/mc-mods/bamboo-flute"},
+//Projects array
+var projectsList = [
+    {date: '2023-02-02', name: "bamboo-flute", link: ""},
+    {date: '2022-05-02', name: "chef-shooter", link: ""},
+    {date: '2020-01-01', name: "minecraft-server", link: ""},
+    {date: '2022-03-26', name: "de_jaffa", link: ""},
+    {date: '2021-10-17', name: "Zombie Body Parts Resourcepack", link: "https://www.planetminecraft.com/texture-pack/zombie-body-parts/"},
+    {date: '2022-08-16', name: "Rain Temple Datapack", link: "https://www.planetminecraft.com/data-pack/rain-temple/"},
+    {date: '2022-08-22', name: "Simple Baking Datapack", link: "https://www.planetminecraft.com/data-pack/simple-baking/"},
+    {date: '2022-10-25', name: "Halloween Decorations Datapack", link: "https://www.planetminecraft.com/data-pack/holiday-spirit-halloween-edition/"},
+    {date: '2022-10-27', name: "Jumpscare Datapack", link: "https://www.planetminecraft.com/data-pack/jumpscare-traps-datapack/"},
+    {date: '2021-05-04', name: "More Ice Mod", link: "https://www.curseforge.com/minecraft/mc-mods/more-ice"},
+    {date: '2022-11-28', name: "Vineyard Mod", link: "https://www.curseforge.com/minecraft/mc-mods/vineyard-mod"},
+    {date: '2022-12-15', name: "Mulberry Mod", link: "https://www.curseforge.com/minecraft/mc-mods/mulberry"},
+    {date: '2023-01-13', name: "Kill Me Not Mod", link: "https://www.curseforge.com/minecraft/mc-mods/kill-me-not"},
 
 
+    {date: '2022-09-14', name: "Minecraft Wipeout", link: "https://www.reddit.com/r/MinecraftCommands/comments/xe7bg2/i_made_a_working_wipeout_course_for_a_youtube/?utm_source=share&utm_medium=web2x&context=3"},
+    {date: '2022-09-11', name: "Album Covers in Minecraft", link: "https://www.reddit.com/r/Minecraft/comments/yqw87q/my_friends_and_i_recreated_a_bunch_of_album/"},
+
+    {date: '2021-04-27', name: "Valorant in Minecraft", link: "https://www.reddit.com/r/Minecraft/comments/mzq7av/i_created_jett_and_sova_from_valorant_in_minecraft/"},
 
 
-    //{time: 0, date: 0, pride: 0, popularity: 0,  name: "", link: ""},
-    //{time: 0, date: 0, pride: 0, popularity: 0,  name: "", link: ""},
-    //{time: 0, date: 0, pride: 0, popularity: 0,  name: "", link: ""},
+    // {date: 'YYYY-DD-MM', name: "Project Name", link: ""},
+    // {date: 'YYYY-DD-MM', name: "Project Name", link: ""}
 ]
 
-//get dropdown list
+//get sorting way list
 var project_sort = document.querySelector('#sorting');
 
 project_sort.addEventListener('change', function() {
@@ -44,58 +40,60 @@ project_sort.addEventListener('change', function() {
 });
 
 function sortProjects() {
-    //clear HTML div
-    document.getElementById("projects_list").innerHTML = "";
-    //sort
-    console.log("SORTING BY:", project_sort.value);
-
-    //TIME
-    if (project_sort.value == "time") {
-        projectList.sort(function(a, b) {
-            return a.time - b.time;
-        })
-    }
-    //DATE
-    if (project_sort.value == "date") {  
-        projectList.sort(function(a, b) {
-            return a.date - b.date;
-        })
-    }
-    //PRIDE
-    if (project_sort.value == "pride") {
-        projectList.sort(function(a, b) {
-            return a.pride - b.pride;
-        })
-    }
-    //POPULARITY
-    if (project_sort.value == "popularity") {
-        projectList.sort(function(a, b) {
-            return a.popularity - b.popularity;
-        })
+    //sort date
+    if (project_sort.value == "newest") {
+        projectsList.sort((a, b) => new Date(a.date) - new Date(b.date));
+        projectsList.reverse();
+        console.log("Sorting by newest!")
+    } else {
+        projectsList.sort((a, b) => new Date(a.date) - new Date(b.date));
+        console.log("Sorting by oldest!")
     }
 
-
-    //reverse (fix) & display list
-    projectList.reverse();
+    console.log('-----------')
+    //display projects
+    for (let i = 0; i < projectsList.length; i++) {
+        console.log(projectsList[i].date)
+    }
     displayProjects();
 }
 
-
 function displayProjects() {
-    for (i = 0; i < projectList.length; i++) {
+    //remove old entries
+    while (gridParent.firstChild) {
+        gridParent.removeChild(gridParent.firstChild);
+    }
 
-        //create img variable & set it to the correct path
+    //loop through all project entries
+    for (i = 0; i < projectsList.length; i++) {
+
+        //Create the div and name it
+        let child = document.createElement('div');
+        child.classList.add('grid-item');
+        child.textContent = (projectsList[i].name).replace('-', ' ');
+
+        //Create img & set it to the correct path
         var img = document.createElement("img");
-        img.src = "assets/thumbnails/" + projectList[i].name + ".png";
-        if (img.naturalWidth < 1) {
-            img.src = "assets/thumbnails/placeHolder.png";
+        img.src = "../../projects/banners/" + projectsList[i].name + ".png";
+
+        //Create the <a> link
+        let button = document.createElement('a');
+        
+        //If the project has a blog page
+        if (projectsList[i].link == '') {
+            button.href = "../../projects/" + projectsList[i].name + ".html"; 
+        } else {
+            button.target = "_blank";
+            button.href = projectsList[i].link;
+
+            //placeholder image
+            img.src = "../../projects/banners/placeHolder.png";
         }
 
-        //draw the img, space, name of project then breakline
-        //DRAW IMG
-        //document.getElementById("projects_list").appendChild(img);
-        document.getElementById("projects_list").innerHTML += (i + 1) + ". "
-        document.getElementById("projects_list").innerHTML += ' ' + '<a href="' + projectList[i].link + '" target="_blank">' + projectList[i].name + '</a>' + '<br>'
 
+        //Add each child to its parent: img -> text -> link -> grid
+        button.appendChild(child);
+        child.appendChild(img);
+        gridParent.appendChild(button);
     }
 }
